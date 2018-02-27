825b730132c9d50fab83a740d55505b9b32d1448
balodi <balodi@gmail.com>
2018-02-27 22:58:04 +0530

    server- testcase passed- server returns a pong for a ping request

--- server.test.js
+++ server.test.js
@@ -6,3 +6,17 @@ describe('test server.js  ', () => {
     expect(server).toBeInstanceOf(Hapi.Server);
   });
 });
+describe('test server  ', () => {
+  test('server should return statuscode 200', (done) => {
+    server.inject('/ping', (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('server should return pong for a ping request', (done) => {
+    server.inject('/ping', (response) => {
+      expect(response.result).toBe('pong');
+      done();
+    });
+  });
+});
